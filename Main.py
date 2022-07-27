2eab5a70252b1da17ddeff14b60a4535050dae38
jeniferj-30 <109952344+jeniferj-30@users.noreply.github.com>
2022-07-27 15:30:10 +0530

    second commit

--- Main.py
+++ Main.py
@@ -1,62 +1,62 @@
 class Evaluate:
-  """This class validates and evaluate postfix expression.
-  Attributes:
-      top: An integer which denotes the index of the element at the top of the stack currently.
-      size_of_stack: An integer which represents the size of stack.
-      stack: A List which acts as a Stack.
-  """
+  #This class validates and evaluate postfix expression.
+  #Attributes:
+      #top: An integer which denotes the index of the element at the top of the stack currently.
+      #size_of_stack: An integer which represents the size of stack.
+      #stack: A List which acts as a Stack.
+
     
 
 
   def __init__(self, size):
-    """Inits Evaluate with top, size_of_stack and stack.
-    Arguments:
-      size_of_stack: An integer to set the size of stack.
-    """
+    #Inits Evaluate with top, size_of_stack and stack.
+    #Arguments:
+      #size_of_stack: An integer to set the size of stack.
+    
     self.top = -1
     self.size_of_stack = size
     self.stack = []
 
 
   def isEmpty(self):
-    """
-    Check whether the stack is empty.
-    Returns:
-      True if it is empty, else returns False.
-    """
+    
+    #Check whether the stack is empty.
+   #Returns:
+      #True if it is empty, else returns False.
+ 
       return len(self.stack) == 0
 
 
   def pop(self):
-    """
-    Do pop operation if the stack is not empty.
-    Returns:
-      The data which is popped out if the stack is not empty.
-    """
+    
+    #Do pop operation if the stack is not empty.
+    #Returns:
+      #The data which is popped out if the stack is not empty.
+    
     if not self.isEmpty():
       self.top-=1
       return self.stack.pop(-1)
 
 
   def push(self, operand):
-    """
-    Push the operand to stack if the stack is not full.
-    Arguments:
-      operand: The operand to be pushed.
-    """
+    
+    #Push the operand to stack if the stack is not full.
+    #Arguments:
+      #operand: The operand to be pushed.
+    
     if len(self.stack)<self.size_of_stack:
       self.top+=1
       self.stack.append(operand)
 
 
   def validate_postfix_expression(self, expression):
-    """
-    Check whether the expression is a valid postfix expression.
-    Arguments:
-      expression: A String which represents the expression to be validated.
-    Returns:
-      True if the expression is valid, else returns False.
-    """
+    
+    #Check whether the expression is a valid postfix expression.
+    #Arguments:
+      #expression: A String which represents the expression to be validated.
+    #Returns:
+      #True if the expression is valid, else returns False.
+    
     operands=[element for element in expression if element.idigit()]
     operators=[element for element in expression if element in ["+","-","*","/","^"]]
     if(len(operands)+len(operators))==len(expression)+len(operandds)==len(operators)+1:
@@ -64,13 +64,13 @@ class Evaluate:
 
 
   def evaluate_postfix_expression(self, expression):
-    """
-    Evaluate the postfix expression
-    Arguments:
-      expression: A String which represents the the expression to be evaluated
-    Returns:
-      The result of evaluated postfix expression.
-    """
+    
+    #Evaluate the postfix expression
+    #Arguments:
+      #expression: A String which represents the the expression to be evaluated
+    #Returns:
+      #The result of evaluated postfix expression.
+    
     self.stack=[]
     for element in expression:
       if element.isdigit():
